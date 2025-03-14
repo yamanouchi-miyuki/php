3dcaf20ed445eb6a328894f1ff3913604af8fb4b
yamanouchi-miyuki <miyukiyamanouchi483@gmail.com>
2025-03-14 09:33:35 +0900

    リモートリポジトリの変更

---
+++ src/php01/index08.php
@@ -0,0 +1,11 @@
+<?php
+$scores = [
+    ["english" => 90, "math" => 88, "science" => 80],
+    ["english" => 64, "math" => 72, "science" => 72],
+    ["english" => 90, "math" => 88, "science" => 80]
+];
+$math_total = 0;
+for ($score=0; $i < count($scores); $i++) { 
+    $score = scores[$i];
+    
+}
